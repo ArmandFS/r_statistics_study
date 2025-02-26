6b4280929f912b73875e06cf2665b4049b480047
Armand Faris A Surbakti <actuallyarmand@gmail.com>
2025-02-26 15:49:22 +1300

    Update binom_distribution.R

--- binom_distribution.R
+++ binom_distribution.R
@@ -9,4 +9,17 @@ dbinom(x=2, size=10, prob = 1/7)
 #pbinom(q, size, prob)
 
 #q is the outcome for which we wish to find P (x < q)
+#q will have the value of 3.
 pbinom(q=3, size=10, prob=1/7)
+
+
+#lets create more distributions
+dbinom(x=3, size=12, prob=1/9)
+
+#P(x >= 3) = 1
+1 - pbinom(q=2, size=10, prob = 1/7)
+
+?dbinom
+
+?pbinom
+
